6f9452a5d04b50a08905ff220bbc745a7c0afb80
seyeoyadiran <seyeoyadiran@gmail.com>
2024-11-05 15:24:21 -0500

    Update and works nice

--- Part1/dist/Part1.e31bb0bc.js
+++ Part1/dist/Part1.e31bb0bc.js
@@ -12528,7 +12528,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64963" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58429" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
